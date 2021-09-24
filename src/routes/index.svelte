32eacf91b21e8fe90f7dc723c87622075e6a4d0f
Dan "Deebster" Smith <29483+Deebster@users.noreply.github.com>
2021-09-24 09:57:19 +0700

    Add dynamic content to homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	let currentTime = new Date().toISOString();
+</script>
+
 <h1>Pages test</h1>
 <ul>
 	<li><a href="./foo">foo</a></li>
 	<li><a href="./deer">deer</a></li>
 </ul>
+<p>This page was generated on {currentTime}</p>
